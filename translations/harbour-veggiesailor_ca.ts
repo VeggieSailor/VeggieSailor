27f30070fca11536bcaab1c3b707ad611e377dd9
bluszcz <bluszcz@bluszcz.net>
2015-03-28 18:02:35 +0100

    More translations

---
+++ translations/harbour-veggiesailor_ca.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ca" version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="37"/>
+        <source>Veggie Sailor</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>Credits</name>
+    <message>
+        <location filename="../qml/pages/Credits.qml" line="12"/>
+        <source>Credits</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/Credits.qml" line="16"/>
+        <source>Veggie Sailor</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/Credits.qml" line="45"/>
+        <source>If you like the app - please support us, we need developers, translators, content editors and more :)</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>Entries</name>
+    <message>
+        <location filename="../qml/pages/Entries.qml" line="22"/>
+        <location filename="../qml/pages/Entries.qml" line="34"/>
+        <source>Add to favorites</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/Entries.qml" line="30"/>
+        <source>Remove from favorites</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/Entries.qml" line="115"/>
+        <source>Unknown</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>EntryMap</name>
+    <message>
+        <location filename="../qml/pages/EntryMap.qml" line="34"/>
+        <source>Address</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/EntryMap.qml" line="40"/>
+        <source>Phone</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>Fav</name>
+    <message>
+        <location filename="../qml/pages/Fav.qml" line="40"/>
+        <source>Favourites</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/Fav.qml" line="49"/>
+        <source>Places</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/Fav.qml" line="125"/>
+        <source>Cities</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="12"/>
+        <source>Clear cache</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="17"/>
+        <source>Credits</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="21"/>
+        <source>Sail</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="25"/>
+        <source>Favorites</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="43"/>
+        <source>Veggie Sailor</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="104"/>
+        <source>Powered by VegGuide.org</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>PlaceInfo</name>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="38"/>
+        <source>Add to favorites</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="107"/>
+        <source>Veg Level</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="115"/>
+        <source>Address</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="134"/>
+        <source>Phone</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="139"/>
+        <source>Description</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="144"/>
+        <source>Price Range</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="149"/>
+        <source>Hours</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="154"/>
+        <source>Cuisines</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="158"/>
+        <source>Tags</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <location filename="../qml/pages/SecondPage.qml" line="42"/>
+        <source>Nested Page</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/SecondPage.qml" line="49"/>
+        <source>Item</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
---
+++ translations/harbour-veggiesailor_pl.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="37"/>
+        <source>Veggie Sailor</source>
+        <translation>Wege  Żeglarz</translation>
+    </message>
+</context>
+<context>
+    <name>Credits</name>
+    <message>
+        <location filename="../qml/pages/Credits.qml" line="12"/>
+        <source>Credits</source>
+        <translation>Autorzy programu</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Credits.qml" line="16"/>
+        <source>Veggie Sailor</source>
+        <translation>Wege  Żeglarz</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Credits.qml" line="45"/>
+        <source>If you like the app - please support us, we need developers, translators, content editors and more :)</source>
+        <translation>Jeśli lubisz tą aplikację - pomoż nam :) potrzebujemy programistów i tłumaczy - ale również kontent edytorów.</translation>
+    </message>
+</context>
+<context>
+    <name>Entries</name>
+    <message>
+        <location filename="../qml/pages/Entries.qml" line="22"/>
+        <location filename="../qml/pages/Entries.qml" line="34"/>
+        <source>Add to favorites</source>
+        <translation>Dodaj do ulubionych</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Entries.qml" line="30"/>
+        <source>Remove from favorites</source>
+        <translation>Usuń z ulubionych</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Entries.qml" line="115"/>
+        <source>Unknown</source>
+        <translation>Nieznany</translation>
+    </message>
+</context>
+<context>
+    <name>EntryMap</name>
+    <message>
+        <location filename="../qml/pages/EntryMap.qml" line="34"/>
+        <source>Address</source>
+        <translation>Adres</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EntryMap.qml" line="40"/>
+        <source>Phone</source>
+        <translation>Telefon</translation>
+    </message>
+</context>
+<context>
+    <name>Fav</name>
+    <message>
+        <location filename="../qml/pages/Fav.qml" line="40"/>
+        <source>Favourites</source>
+        <translation>Ulubione</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Fav.qml" line="49"/>
+        <source>Places</source>
+        <translation>Miejsca</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Fav.qml" line="125"/>
+        <source>Cities</source>
+        <translation>Miasta</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="12"/>
+        <source>Clear cache</source>
+        <translation>Wyczyść pamięć podręczną</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="17"/>
+        <source>Credits</source>
+        <translation>Autorzy programu</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="21"/>
+        <source>Sail</source>
+        <translation>Żegluj</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="25"/>
+        <source>Favorites</source>
+        <translation>Ulubione</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="43"/>
+        <source>Veggie Sailor</source>
+        <translation>Wege  Żeglarz</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="104"/>
+        <source>Powered by VegGuide.org</source>
+        <translation>Używamy VegGuide.org API</translation>
+    </message>
+</context>
+<context>
+    <name>PlaceInfo</name>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="38"/>
+        <source>Add to favorites</source>
+        <translation>Dodaj do ulubionych</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="107"/>
+        <source>Veg Level</source>
+        <translation>Wege poziom</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="115"/>
+        <source>Address</source>
+        <translation>Adres</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="134"/>
+        <source>Phone</source>
+        <translation>Telefon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="139"/>
+        <source>Description</source>
+        <translation>Opis</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="144"/>
+        <source>Price Range</source>
+        <translation>Zakres cenowy</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="149"/>
+        <source>Hours</source>
+        <translation>Godziny</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="154"/>
+        <source>Cuisines</source>
+        <translation>Kuchnie</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PlaceInfo.qml" line="158"/>
+        <source>Tags</source>
+        <translation>Tagi</translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <location filename="../qml/pages/SecondPage.qml" line="42"/>
+        <source>Nested Page</source>
+        <translation>Zagnieżdżona strona</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SecondPage.qml" line="49"/>
+        <source>Item</source>
+        <translation>Element</translation>
+    </message>
+</context>
+</TS>
